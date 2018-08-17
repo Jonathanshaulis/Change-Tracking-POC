a77588ce313a2d741f8ea5b6eceba196e4969e6a
jonathanshaulis@gmail.com <jonathanshaulis@gmail.com>
2018-08-17 16:59:54 -0600

    Removed unnecessary testing tables
    Removed tables 3-5 and their testing results.

--- 6-Create Test Records.sql
+++ 6-Create Test Records.sql
@@ -25,38 +25,6 @@ VALUES
 ('B', 2, '2018-02-01'),
 ('C', 3, '2018-03-01');
 
-INSERT INTO TestRecords3
-(
-    FakeVarchar,
-    FakeInt,
-    FakeDate
-)
-VALUES
-('A', 1, '2018-01-01'),
-('B', 2, '2018-02-01'),
-('C', 3, '2018-03-01');
-
-INSERT INTO TestRecords4
-(
-    FakeVarchar,
-    FakeInt,
-    FakeDate
-)
-VALUES
-('A', 1, '2018-01-01'),
-('B', 2, '2018-02-01'),
-('C', 3, '2018-03-01');
-
-INSERT INTO TestRecords5
-(
-    FakeVarchar,
-    FakeInt,
-    FakeDate
-)
-VALUES
-('A', 1, '2018-01-01'),
-('B', 2, '2018-02-01'),
-('C', 3, '2018-03-01');
 
 
 
